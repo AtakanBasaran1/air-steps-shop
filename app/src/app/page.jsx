4cbda5e282ca90cedb6b8b19cc38e9d41ed16951
Atakan <158854874+Atakanlift@users.noreply.github.com>
2024-09-28 00:20:26 +0300

    1
    1

---
+++ app/src/app/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Page = () => {
+  return (
+    <div className='container d-flex justify-content-center align-items-center'>
+      <h1 className='text-center mt-[300px] font-bold'>ÇOK YAKINDA SİZLERLEYİZ!</h1>
+      <p className='text-center text-[20px] mt-10 font-semibold'>@airstepshop</p>
+    </div>
+  )
+}
+
+export default Page;
